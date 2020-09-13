f4cddc5365232ed63171905ecb6e62a9bada79ce
Johnathan Baird <janimationd@gmail.com>
2020-09-13 08:45:24 -0700

    Adding project to repo

---
+++ Source/EndOfRuin/EndOfRuin.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EndOfRuin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EndOfRuin, "EndOfRuin" );
---
+++ Source/EndOfRuin/Public/CodeUtilities.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CodeUtilities.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDOFRUIN_API UCodeUtilities : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
